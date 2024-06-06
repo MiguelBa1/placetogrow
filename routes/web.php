a29d2df846f81eb67f92e309f71168f4d37059ab
Miguel Barrera <miguelbarreraa05@gmail.com>
2024-06-06 09:20:32 -0500

    feat: integrate Laravel Breeze into the project

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,27 @@
 <?php
 
-use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', fn() => Inertia::render('Index'))->name('index');
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-Route::get('/site1', [PaymentController::class, 'index'])->name('site1');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/site1', [PaymentController::class, 'store'])->name('site1.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/site1/return/{reference}', [PaymentController::class, 'return'])->name('site1.return');
+require __DIR__.'/auth.php';
