f8351baea4451d7f1fcc81ab5e17eec62cd0a617
Miguel Barrera <miguelbarreraa05@gmail.com>
2024-06-19 14:01:10 -0500

    refactor(auth): remove UI and code for user creation

--- resources/js/locales/en/auth/index.ts
+++ resources/js/locales/en/auth/index.ts
@@ -1,7 +1,6 @@
 import { enConfirmPassword } from './ConfirmPassword';
 import { enForgotPassword } from './ForgotPassword';
 import { enLogin } from './Login';
-import { enRegister } from './Register';
 import { enResetPassword } from './ResetPassword';
 import { enVerifyEmail } from './VerifyEmail';
 
@@ -9,7 +8,6 @@ export default {
     confirmPassword: enConfirmPassword,
     forgotPassword: enForgotPassword,
     login: enLogin,
-    register: enRegister,
     resetPassword: enResetPassword,
     verifyEmail: enVerifyEmail,
 };
--- resources/js/locales/es/auth/Register.ts
+++
@@ -1,9 +0,0 @@
-export const esRegister = {
-    title: "Registrarse",
-    nameLabel: "Nombre",
-    emailLabel: "Correo Electrónico",
-    passwordLabel: "Contraseña",
-    confirmPasswordLabel: "Confirmar Contraseña",
-    alreadyRegistered: "¿Ya estás registrado?",
-    registerButton: "Registrarse",
-}
