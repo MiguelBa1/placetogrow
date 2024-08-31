cd00a6defafd0438e9f75eb57c91ac24fa55e48f
Miguel Barrera <miguelbarreraa05@gmail.com>
2024-08-31 11:05:44 -0500

    feat: add imports table

---
+++ database/migrations/2024_08_31_034245_create_imports_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Constants\ImportStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('imports', function (Blueprint $table) {
+            $table->id();
+            $table->string('filename');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('status', array_column(ImportStatus::cases(), 'value'))->default(ImportStatus::PENDING->value);
+            $table->json('errors')->nullable();
+            $table->unique(['user_id', 'filename']);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('imports');
+    }
+};
