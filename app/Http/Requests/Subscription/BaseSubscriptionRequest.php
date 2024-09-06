d491204b58a539be2015bad99bd928775dec372e
miguel <miguelbarreraa05@gmail.com>
2024-09-05 23:46:15 -0500

    feat: add validation in BaseSubscriptionRequest

--- app/Http/Requests/Subscription/BaseSubscriptionRequest.php
+++ app/Http/Requests/Subscription/BaseSubscriptionRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Subscription;
 
 use App\Constants\TimeUnit;
+use Closure;
 use Illuminate\Foundation\Http\FormRequest;
 
 abstract class BaseSubscriptionRequest extends FormRequest
@@ -12,7 +13,7 @@ abstract class BaseSubscriptionRequest extends FormRequest
         return [
             'price' => ['numeric'],
             'total_duration' => ['integer'],
-            'billing_frequency' => ['integer'],
+            'billing_frequency' => ['integer', fn ($attribute, $value, $fail) => $this->validateDates($fail)],
             'time_unit' => ['in:' . implode(',', TimeUnit::toArray())],
             'translations' => ['array'],
             'translations.*.locale' => ['string', 'max:2'],
@@ -27,5 +28,21 @@ abstract class BaseSubscriptionRequest extends FormRequest
             'translations.*.name' => __('validation.attributes.name'),
             'translations.*.description' => __('validation.attributes.description'),
         ];
+    }
+
+    public function validateDates(Closure $fail): bool
+    {
+        $totalDuration = $this->get('total_duration');
+        $billingFrequency = $this->get('billing_frequency');
+
+        if ($totalDuration === null || $billingFrequency === null) {
+            return true;
+        }
+
+        if ($totalDuration % $billingFrequency !== 0) {
+            $fail(__('validation.custom.subscription.billing_frequency'));
+        }
+
+        return true;
     }
 }
--- lang/en/validation.php
+++ lang/en/validation.php
@@ -5,6 +5,9 @@ return [
         'validation_rules' => [
             'invalid' => 'The field contains invalid validation rules.',
         ],
+        'subscription' => [
+            'billing_frequency' => 'The billing frequency must be a divisor of the total duration.',
+        ],
     ],
     'attributes' => [
         'name' => 'name',
