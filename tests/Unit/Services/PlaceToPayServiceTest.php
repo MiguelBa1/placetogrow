20bf5bcaa54e9709a22cd999a1609025d8841002
miguel <miguelbarreraa05@gmail.com>
2024-09-07 14:10:36 -0500

    feat: add subscriptions methods for PlaceToPayServiceTest

--- tests/Unit/Services/PlaceToPayServiceTest.php
+++ tests/Unit/Services/PlaceToPayServiceTest.php
@@ -7,6 +7,7 @@ use App\Constants\DocumentType;
 use App\Constants\PaymentStatus;
 use App\Constants\PlaceToPayStatus;
 use App\Models\Customer;
+use App\Models\CustomerSubscription;
 use App\Models\Payment;
 use App\Services\PlaceToPayService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -18,7 +19,7 @@ class PlaceToPayServiceTest extends TestCase
 {
     use RefreshDatabase, PlaceToPayMockTrait;
 
-    public function test_can_create_a_payment()
+    public function test_can_create_a_payment(): void
     {
         Http::fake([
             config('payments.placetopay.url') => Http::response(['status' => PlaceToPayStatus::OK->value]),
@@ -50,7 +51,7 @@ class PlaceToPayServiceTest extends TestCase
         $this->assertEquals(PlaceToPayStatus::OK->value, $response->json()['status']);
     }
 
-    public function test_can_check_a_payment()
+    public function test_can_check_a_payment(): void
     {
         Http::fake([
             config('payments.placetopay.url') => Http::response(['status' => PaymentStatus::APPROVED->value]),
@@ -59,6 +60,52 @@ class PlaceToPayServiceTest extends TestCase
         $service = new PlaceToPayService();
 
         $response = $service->checkPayment('test_session_id');
+
+        $this->assertEquals(200, $response->status());
+
+        $this->assertArrayHasKey('status', $response->json());
+        $this->assertEquals(PaymentStatus::APPROVED->value, $response->json()['status']);
+    }
+
+    public function test_can_create_a_subscription(): void
+    {
+        Http::fake([
+            config('payments.placetopay.url') => Http::response(['status' => PlaceToPayStatus::OK->value]),
+        ]);
+
+        $customer = Customer::factory()->create([
+            'name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'test@mail.com',
+            'document_type' => DocumentType::CC->value,
+            'document_number' => '1234567890',
+            'phone' => '1234567890',
+        ]);
+
+        $subscription = CustomerSubscription::factory()->create([
+            'reference' => 'test_reference',
+            'description' => 'Test Subscription',
+        ]);
+
+        $service = new PlaceToPayService();
+
+        $response = $service->createSubscription($customer, $subscription);
+
+        $this->assertEquals(200, $response->status());
+
+        $this->assertArrayHasKey('status', $response->json());
+        $this->assertEquals(PlaceToPayStatus::OK->value, $response->json()['status']);
+    }
+
+    public function test_can_check_a_subscription(): void
+    {
+        Http::fake([
+            config('payments.placetopay.url') => Http::response(['status' => PaymentStatus::APPROVED->value]),
+        ]);
+
+        $service = new PlaceToPayService();
+
+        $response = $service->checkSubscription('test_session_id');
 
         $this->assertEquals(200, $response->status());
 
