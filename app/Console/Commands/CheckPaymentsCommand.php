2212e44d27441c4ebafa4dc31280994954a7f195
miguel <miguelbarreraa05@gmail.com>
2024-10-04 17:37:12 -0500

    refactor(commands): move payment status check to a job

--- app/Console/Commands/CheckPaymentsCommand.php
+++ app/Console/Commands/CheckPaymentsCommand.php
@@ -3,38 +3,31 @@
 namespace App\Console\Commands;
 
 use App\Constants\PaymentStatus;
-use App\Contracts\PaymentServiceInterface;
+use App\Jobs\CheckPaymentStatusJob;
 use App\Models\Payment;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Cache;
 
 class CheckPaymentsCommand extends Command
 {
-    private PaymentServiceInterface $paymentService;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->paymentService = app(PaymentServiceInterface::class);
-    }
-
     protected $signature = 'app:check-payments';
 
     protected $description = 'Command to check the status of the payments';
 
     public function handle(): void
     {
-        $payments = Payment::query()->where('status', PaymentStatus::PENDING->value)
+        $payments = Payment::query()
+            ->where('status', PaymentStatus::PENDING->value)
             ->where('created_at', '<', now()->subMinutes(config('payments.check_interval_minutes')))
             ->get();
 
-        foreach ($payments as $payment) {
-            $this->info('Checking payment ' . $payment->request_id);
-            $isSuccessful = $this->paymentService->checkPayment($payment);
+        if ($payments->isEmpty()) {
+            $this->info('No payments to check');
+            return;
+        }
 
-            if ($isSuccessful) {
-                Cache::forget('payment_checked_' . $payment->id);
-            }
+        foreach ($payments as $payment) {
+            $this->info('Dispatching job to check payment ' . $payment->request_id);
+            CheckPaymentStatusJob::dispatch($payment);
         }
     }
 }
