514cdf5104ab12b1b91fb2e75321cf22e2375fad
miguel <miguelbarreraa05@gmail.com>
2024-10-11 21:44:04 -0500

    refactor: separate DashboardController in action

---
+++ app/Actions/Dashboard/GetDashboardDataAction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Actions\Dashboard;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+
+class GetDashboardDataAction
+{
+    public function execute(): array
+    {
+        $data = [
+            'paymentsOverTime' => Cache::remember('payments_over_time', 86400, function () {
+                return DB::select('CALL get_payments_over_time()');
+            }),
+            'topMicrositesByTransactions' => Cache::remember('top_microsites_by_transactions', 86400, function () {
+                return DB::select('CALL get_top_microsites_by_transactions()');
+            }),
+            'invoiceDistribution' => Cache::remember('invoice_distribution', 86400, function () {
+                return DB::select('CALL get_invoice_distribution()');
+            }),
+            'subscriptionDistribution' => Cache::remember('subscription_distribution', 86400, function () {
+                return DB::select('CALL get_subscription_distribution()');
+            }),
+            'approvedTransactionsByMicrositeType' => Cache::remember('approved_transactions_by_microsite_type', 86400, function () {
+                return DB::select('CALL get_approved_transactions_by_microsite_type()');
+            }),
+        ];
+
+        $lastUpdated = Cache::get('dashboard_cache_timestamp', Carbon::now()->toDateTimeString());
+
+        Cache::put('dashboard_cache_timestamp', Carbon::now()->toDateTimeString(), 86400);
+
+        return [
+            'data' => $data,
+            'lastUpdated' => $lastUpdated
+        ];
+    }
+}
--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -2,43 +2,23 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Actions\Dashboard\GetDashboardDataAction;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Dashboard\DashboardDataResource;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class DashboardController extends Controller
 {
-    public function index(): Response
+    public function index(GetDashboardDataAction $getDashboardDataAction): Response
     {
-        $data = [
-            'paymentsOverTime' => Cache::remember('payments_over_time', 86400, function () {
-                return DB::select('CALL get_payments_over_time()');
-            }),
-            'topMicrositesByTransactions' => Cache::remember('top_microsites_by_transactions', 86400, function () {
-                return DB::select('CALL get_top_microsites_by_transactions()');
-            }),
-            'invoiceDistribution' => Cache::remember('invoice_distribution', 86400, function () {
-                return DB::select('CALL get_invoice_distribution()');
-            }),
-            'subscriptionDistribution' => Cache::remember('subscription_distribution', 86400, function () {
-                return DB::select('CALL get_subscription_distribution()');
-            }),
-            'approvedTransactionsByMicrositeType' => Cache::remember('approved_transactions_by_microsite_type', 86400, function () {
-                return DB::select('CALL get_approved_transactions_by_microsite_type()');
-            }),
-        ];
+        $dashboardData = $getDashboardDataAction->execute();
 
-        $lastUpdated = Cache::get('dashboard_cache_timestamp', Carbon::now()->toDateTimeString());
-
-        Cache::put('dashboard_cache_timestamp', Carbon::now()->toDateTimeString(), 86400);
+        $data = (new DashboardDataResource((object) $dashboardData['data']))->toArray(request());
 
         return Inertia::render('Dashboard/Index', [
-            'data' => (new DashboardDataResource((object) $data))->toArray(request()),
-            'lastUpdated' => $lastUpdated,
+            'data' => $data,
+            'lastUpdated' => $dashboardData['lastUpdated']
         ]);
     }
 }
